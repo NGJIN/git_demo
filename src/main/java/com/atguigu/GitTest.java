4a36dc2f3832e4da97d560e5218a273355cd38ff
ynag <yang1111@qq.com>
2021-11-25 21:06:11 +0800

    hot-3 commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello hot");
         System.out.println("hello hot2");
+        System.out.println("hello hot3");
     }
 }
